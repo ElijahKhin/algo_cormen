687e32061764f09a6e0dca24ce48a35a3c7bf27d
ElijahKhin <workbiscap@gmail.com>
2025-02-04 16:47:50 +0300

    minor changes

--- insertion_sort.cc
+++ insertion_sort.cc
@@ -12,16 +12,6 @@ void insertion_sort(std::vector<int>& a) {
 	}
 }
 
-void insertion_sort(std::vector<int>& a) {
-	for (int i = 1; i < (int)a.size(); ++i) {
-		int key = a[i], j = i - 1;
-		while (j >= 0 && key < a[j]) {
-			a[j+1] = a[j];
-			--j;
-		}
-		a[j+1] = key;
-	}
-}
 int main() {
 	std::vector<int> a = {23, 12, 1, 0, -100, 235, 235, 259, 2, 10, 50};	
 	
